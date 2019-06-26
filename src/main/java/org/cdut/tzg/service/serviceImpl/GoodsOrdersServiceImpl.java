8895c2046bac5b736e1db94e30baea5c1e633e2a
anlan <anlan1996@qq.com>
2019-06-26 10:26:25 +0800

    ajax

--- src/main/java/org/cdut/tzg/service/serviceImpl/GoodsOrdersServiceImpl.java
+++ src/main/java/org/cdut/tzg/service/serviceImpl/GoodsOrdersServiceImpl.java
@@ -4,6 +4,7 @@ import org.cdut.tzg.mapper.GoodsOrdersMapper;
 import org.cdut.tzg.model.GoodsOrders;
 import org.cdut.tzg.service.GoodsOrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * @author anlan
  * @date 2019/6/26 9:22
  */
+@Service
 public class GoodsOrdersServiceImpl implements GoodsOrdersService {
 
     @Autowired
