13148a2797b00eaaa4727ba32c4599c26a364f05
Zacky_ran <2441804021@qq.com>
2019-06-28 11:18:17 +0800

    完成 cart goods的api  更新资源请求文档

--- src/main/java/org/cdut/tzg/controller/GoodsController.java
+++ src/main/java/org/cdut/tzg/controller/GoodsController.java
@@ -35,16 +35,16 @@ public class GoodsController {
     private GoodsOrdersService goodsOrdersService;
 
     /**
-     URL：/api/goods/getInfo
-     描述：获取商品信息
-     方法：GET
-     数据：data:{"商品ID":"xxx"}
-     期望返回格式：{"卖家名称":"XXX","卖家头像":"XXX",卖家成功交易量":XXX}
-
-     返回：
-     成功：{"code":200,"msg":"success","data":{"卖家成功交易量":0,"卖家名称":"jack","卖家头像":null}}
-     商品不在商品表中：{"code":500303,"msg":"商品不存在","data":null}
-     商品的卖家不在用户表中：{"code":500201,"msg":"未找到该用户","data":null}
+     *URL：/api/goods/getInfo
+     *描述：获取商品信息
+     *方法：GET
+     *数据：data:{"商品ID":"xxx"}
+     *期望返回格式：{"卖家名称":"XXX","卖家头像":"XXX",卖家成功交易量":XXX}
+     *
+     *返回：
+     *成功：{"code":200,"msg":"success","data":{"卖家成功交易量":0,"卖家名称":"jack","卖家头像":null}}
+     *商品不在商品表中：{"code":500303,"msg":"商品不存在","data":null}
+     *商品的卖家不在用户表中：{"code":500201,"msg":"未找到该用户","data":null}
      */
 
     @RequestMapping(value = "/getInfo",method = RequestMethod.GET)
@@ -67,15 +67,15 @@ public class GoodsController {
 
 
     /**
-     URL：/api/goods/addToCart
-     描述：添加商品到购物车
-     方法：POST
-     数据：{"用户名","XXX","商品ID":"XXX","商品数量":XXX}
-     期望返回格式：{"code":XXX,"msg":"XXXX","data":null}
-
-     返回:
-     成功：{"code":200,"msg":"success","data":"添加到购物车成功"}
-     失败：要添加的商品超出了商品表中的库存：{"code":500301,"msg":"库存不足","data":null}
+     *URL：/api/goods/addToCart
+     *描述：添加商品到购物车
+     *方法：POST
+     *数据：{"用户名","XXX","商品ID":"XXX","商品数量":XXX}
+     *期望返回格式：{"code":XXX,"msg":"XXXX","data":null}
+     *
+     *返回:
+     *成功：{"code":200,"msg":"success","data":"添加到购物车成功"}
+     *失败：要添加的商品超出了商品表中的库存：{"code":500301,"msg":"库存不足","data":null}
      */
     @RequestMapping(value = "/addToCart",method = RequestMethod.POST)
     @Transactional
@@ -116,14 +116,14 @@ public class GoodsController {
 
 
     /**
-     描述：立即购买商品
-     方法：POST
-     数据：{“用户名”:”XXX”,“商品ID”:”XXX”,”数量”:XXX}
-     期望返回格式：{"code":XXX,"msg":"xxx","data":{”订单ID”:”xxxx”,"卖家信息":{"卖家用户名":"XXX",收款码":"XXX","金钱":xxx}}}
-
-     返回：
-     成功：立即购买成功：{"code":200,"msg":"success","data":{"卖家信息":{"收款码":"jack的收款码","卖家用户名":"jack","金钱":50},"订单ID":23}}
-     失败:立即购买的商品库存不足：{"code":500301,"msg":"库存不足","data":null}
+     *描述：立即购买商品
+     *方法：POST
+     *数据：{“用户名”:”XXX”,“商品ID”:”XXX”,”数量”:XXX}
+     *期望返回格式：{"code":XXX,"msg":"xxx","data":{”订单ID”:”xxxx”,"卖家信息":{"卖家用户名":"XXX",收款码":"XXX","金钱":xxx}}}
+     *
+     *返回：
+     *成功：立即购买成功：{"code":200,"msg":"success","data":{"卖家信息":{"收款码":"jack的收款码","卖家用户名":"jack","金钱":50},"订单ID":23}}
+     *失败:立即购买的商品库存不足：{"code":500301,"msg":"库存不足","data":null}
      */
     @RequestMapping(value = "/buyNow",method =RequestMethod.POST)
     @ResponseBody
@@ -182,20 +182,20 @@ public class GoodsController {
 
 
     /**
-     URL：/api/goods/gallery
-     描述：获取所有同标签商品
-     方法：GET
-     数据：{"标签种类":"XXX"}
-     期望返回数据：[{“商品标签”:”XXX”,”商品名称”:”XXX”,”描述”:”XXX”,”单价”:XXX,”数量”:XXX,”卖家ID”:XXX,”商品ID”:XXX,”商品图片”:”XXX”,"联系方式":"XXX"},]
-
-     返回：
-     查询同种类型商品成功：
-     {"code":200,"msg":"success","data":[{"卖家ID":2,"商品名称":"Java","数量":1,"商品ID":2,"单价":64.8,"商品图片":null,"联系方式":"15196684789","商品标签":1,"描述":"Java程序设计"}
-     ,{"卖家ID":2,"商品名称":"C++Primer","数量":2,"商品ID":3,"单价":64.8,"商品图片":null,"联系方式":"15196684789","商品标签":1,"描述":"C++Prime第五版"}
-     ,{"卖家ID":6,"商品名称":"蘑菇蘑菇蘑菇","数量":928,"商品ID":33,"单价":40.0,"商品图片":null,"联系方式":"13512345678","商品标签":1,"描述":"提莫快来买蘑菇"}]}
-
-     失败，没有该类型的商品：{"code":500302,"msg":"当前没有该类型商品","data":null}
-     失败，其中如果有没有卖家的商品 {"code":500201,"msg":"未找到该用户","data":null}
+     *URL：/api/goods/gallery
+     *描述：获取所有同标签商品
+     *方法：GET
+     *数据：{"标签种类":"XXX"}
+     *期望返回数据：[{“商品标签”:”XXX”,”商品名称”:”XXX”,”描述”:”XXX”,”单价”:XXX,”数量”:XXX,”卖家ID”:XXX,”商品ID”:XXX,”商品图片”:”XXX”,"联系方式":"XXX"},]
+     *
+     *返回：
+     *查询同种类型商品成功：
+     *{"code":200,"msg":"success","data":[{"卖家ID":2,"商品名称":"Java","数量":1,"商品ID":2,"单价":64.8,"商品图片":null,"联系方式":"15196684789","商品标签":1,"描述":"Java程序设计"}
+     *,{"卖家ID":2,"商品名称":"C++Primer","数量":2,"商品ID":3,"单价":64.8,"商品图片":null,"联系方式":"15196684789","商品标签":1,"描述":"C++Prime第五版"}
+     *,{"卖家ID":6,"商品名称":"蘑菇蘑菇蘑菇","数量":928,"商品ID":33,"单价":40.0,"商品图片":null,"联系方式":"13512345678","商品标签":1,"描述":"提莫快来买蘑菇"}]}
+     *
+     *失败，没有该类型的商品：{"code":500302,"msg":"当前没有该类型商品","data":null}
+     *失败，其中如果有没有卖家的商品 {"code":500201,"msg":"未找到该用户","data":null}
           */
     @RequestMapping(value = "/gallery",method = RequestMethod.GET)
     @ResponseBody
