c4b0a02c6e7cf2ba666145353c025b939761118b
JCQ <JiangChangq@outlook.com>
2019-07-03 22:40:33 +0800

    后台数据返回格式调整

--- src/main/java/org/cdut/tzg/controller/OrderController.java
+++ src/main/java/org/cdut/tzg/controller/OrderController.java
@@ -37,38 +37,32 @@ public class OrderController {
     private UserService userService;
 
     /*
-    * 描述：获取后台数据
+    * 描述：显示数据信息
     * 方法：GET
-    * 期望返回格式：{"网站活跃度":[xxx,xxx,xxx],"网站日交易量":[xxx,xxx,xxx],"网站总交易量":xxx,"求购标签":[{"标签名1":xxx},{"标签名2":xxx},{"标签名3":xxx},...]}
+    * 期望返回格式：{"网站活跃度":{YYYY-mm-dd:xxx,YYYY-mm-dd:xxx},"网站日交易量":[YYYY-mm-dd:xxx,YYYY-mm-dd:xxx],"网站总交易量":xxx,"求购标签":[{"标签名1":xxx},{"标签名2":xxx},{"标签名3":xxx},...]}
     * 返回值：
-    * {code: 200, msg: "success", data: {"网站活跃度":[12,23,34],"网站日交易量":[12,23,34],"网站总交易量":800,"求购标签":[{"标签名1":111},{"标签名2":222},{"标签名3":333},...]}}
-    */
+    * {code: 200, msg: "success", data: {"网站活跃度":[2019-7-2:12,2019-7-3:23],"网站日交易量":[2019-7-2:12,2019-7-3:23],"网站总交易量":800,"求购标签":[{"标签名1":111},{"标签名2":222},{"标签名3":333},...]}}
+    * */
     @RequestMapping(value = "/getData",method = RequestMethod.POST)
     @ResponseBody
     public Result<Map<String, Object>> getAllOrders(){
         //返回数据载体
         Map<String,Object> map = new HashMap<>();
         //存储每日活跃量、销售量
-        //Integer[] active = new Integer[7];
         Map<String,Integer> active = new HashMap<>();
         Map<String,Integer> daySold = new HashMap<>();
-        //Integer[] daySold = new Integer[7];
 
         //获取一周内日期
-        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar calendar = Calendar.getInstance();
         Date date = new Date();
         calendar.setTime(date);
         //统计前七日的活跃度、交易量
         for(int i=7;i>0;i--){
             calendar.add(calendar.DAY_OF_MONTH,-i);
-            //System.out.println(sdf.format(calendar.getTime()));
             int orderCount = goodsService.getGoodsCount(calendar.getTime());
             int daySoldCount = orderService.getCompletedOrdersCount(calendar.getTime());
             active.put(String.format("%tY-%tm-%td",calendar,calendar,calendar),orderCount);
             daySold.put(String.format("%tY-%tm-%td",calendar,calendar,calendar),daySoldCount);
-            //active[7-i] = orderCount;
-            //daySold[7-i] = daySoldCount;
             calendar.setTime(date);
         }
         //统计各种求购标签的商品数量
