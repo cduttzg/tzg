384f07bca3d80304100fecba2c2c65fc6de52f97
anlan <anlan1996@qq.com>
2019-07-05 08:21:28 +0800

    Ok

---
+++ src/main/resources/res/precache-manifest.474f09a445b80a230f96c12cb252e58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081228a471aaf88d925916b018460348",
+    "url": "/res/index.html"
+  },
+  {
+    "revision": "e9a8c84579c8f2930db3",
+    "url": "/res/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "30e04501df5774aedaec",
+    "url": "/res/static/css/main.c56885fa.chunk.css"
+  },
+  {
+    "revision": "e9a8c84579c8f2930db3",
+    "url": "/res/static/js/2.c4a5024f.chunk.js"
+  },
+  {
+    "revision": "30e04501df5774aedaec",
+    "url": "/res/static/js/main.520aee3f.chunk.js"
+  },
+  {
+    "revision": "d0a0b3fa203560975090",
+    "url": "/res/static/js/runtime~main.844863f4.js"
+  }
+]);
